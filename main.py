642e42a0c3b1279550c28d49ab4422c21a22ed92
techwithguga <guramxatiashvili@hotmail.com>
2024-02-02 16:04:26 +0000

    add operation functions, save them in a dictionary

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 # + - * / calculator
+
+def add(n1,n2):
+    return n1+n2
+
+def subtract(n1,n2):
+    return n1-n2
+
+def multiply(n1,n2):
+    return n1*n2
+
+def divide(n1,n2):
+    return n1/n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+print(operations)
