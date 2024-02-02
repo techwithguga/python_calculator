3adc9df95f5ae1105e57d46493f1cf3ad82a86e1
techwithguga <guramxatiashvili@hotmail.com>
2024-02-02 16:35:12 +0000

    call operation function from dictionary based on the user input

--- main.py
+++ main.py
@@ -19,4 +19,19 @@ operations = {
     "/": divide
 }
 
-print(operations)
+# multiply_function = operations["*"]
+# print(multiply_function(2,3))
+
+num1 = int(input("what's the first nymber?: "))
+num2 = int(input("what's the second nymber?: "))
+
+for symbol in operations:
+    print(symbol)
+
+operation_symbol = input("type an operation from the line above: ")
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1,num2)
+
+print(f'{num1} {operation_symbol} {num2} = {answer}')
+    
+            
